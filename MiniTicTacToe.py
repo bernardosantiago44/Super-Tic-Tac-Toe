32192c2157f089fc9db1d7bc8149a15958ebf9b2
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2023-10-11 13:51:30 -0600

    Created MiniTicTacToe game class.

--- MiniTicTacToe.py
+++ MiniTicTacToe.py
@@ -1,1 +1,17 @@
+from TicTacToeBase import TicTacToeBase
+from numpy import where
 
+class MiniTicTacToe(TicTacToeBase):
+    def __init__(self, gamePosition):
+        super().__init__()
+        self.gamePosition = gamePosition
+        index = where(self.boardPositions == gamePosition)
+        self.__row = index[0][0]
+        self.__column = index [1][0]
+
+    def showBoard(self):
+        print(f'                   Column {self.__column} Row {self.__row}')
+        return super().showBoard()
+
+miniGame = MiniTicTacToe(7)
+miniGame.showBoard()
